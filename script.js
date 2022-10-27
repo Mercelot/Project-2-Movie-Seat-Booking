e2dd0b01be75f8bba8b7bf4ce5b9afb4c04e8b20
shane <shaneaudinett@gmail.com>
2022-10-27 13:00:06 +0100

    Finalised seat and update functions for price.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
@@ -13,10 +13,16 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+// Seat click event
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
     }
-
     updateSelectedCount();
 })
